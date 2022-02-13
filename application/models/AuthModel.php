1a130408e1316b45e5a8aa2918322461289e31ba
Foozaweb <60440000+foozaweb@users.noreply.github.com>
2022-02-13 23:53:46 +0100

    Update AuthModel.php

--- application/models/AuthModel.php
+++ application/models/AuthModel.php
@@ -134,8 +134,8 @@ class AuthModel extends CI_Model
 			'protocol' => 'mail', //sendmail
 			'smtp_host' => 'ssl://smtp.googlemail.com',
 			'smtp_port' => '465',
-			'smtp_user' => 'chbluxuryikeja@gmail.com',
-			'smtp_pass' => 'CHBLuxury@Ikeja12345',
+			'smtp_user' => 'your@gmail.com',
+			'smtp_pass' => 'gmail_password',
 			'smtp_timeout' => 50,
 			'mailtype' => 'html',
 			'starttls'  => true,
